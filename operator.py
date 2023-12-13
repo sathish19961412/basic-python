38c7e18af8bf8385bef7232aef3643078903676d
sathish19961412 <sathishfreelanc5@gmail.com>
2023-12-13 12:44:38 +0530

    pyrhon completed basic operator

---
+++ operator.py
@@ -0,0 +1,38 @@
+'''
+    Arithmetic operators
+    Assignment operators
+    Comparison operators
+    Logical operators
+    Identity operators
+    Membership operators
+    Bitwise operators
+'''
+#Arithmetic operators
+
+a=12
+b=5
+#addition
+print('a+b =',a+b)
+
+#substract
+print('a-b =',a-b)
+
+#multiple
+print('a*b = ',a*b)
+
+#divide
+
+print('a/b =',a/b)
+
+
+#assignment Operator
+
+x=3
+
+x+=5
+
+print(x)
+
+x/=2
+
+print(x)

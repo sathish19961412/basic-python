94c53a8fff479e8a5e0b2c4789f5e8a54440a8c3
sathish19961412 <sathishfreelanc5@gmail.com>
2024-12-17 16:34:34 +0530

    tap

---
+++ tac.py
@@ -0,0 +1,16 @@
+print("Enter Marks the Five Subjects")
+sub1=float(input("Enter Sub1"))
+sub2=float(input("Enter Sub2"))
+sub3=float(input("Enter Sub3"))
+sub4=float(input("Enter Sub4"))
+sub5=float(input("Enter Sub5"))
+
+total=sub1+sub2+sub3+sub4+sub5
+avg=total/5.0
+percentage=(total/500.0)*100
+
+#print The Result
+
+print("Display Total Amount",total)
+print("Display Avg",avg)
+print("Display Percentage",percentage)
